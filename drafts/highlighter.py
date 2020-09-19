2b0b1404b26b61b0a8677639929ae0cc9b8ccefa
VideoCarp <66365570+VideoCarp@users.noreply.github.com>
2020-09-19 17:20:07 +1000

    Update highlighter.py

--- drafts/highlighter.py
+++ drafts/highlighter.py
@@ -1,1 +1,8 @@
 # Python version, to be compiled to JS
+
+not_hl = document.getElementById("tohighlight");
+keyword_list = ["detect", "type=", "c=", "r=", "@e", "@s", "@p", "@r", "name=",\
+                "showcoordinates", "alwaysday", "commandblockoutput", "false",\
+                "true", "immediaterespawn", "falldamage", "dotiledrops", "keepinventory"\
+                , "commandfeedback", "randomtickspeed", "tick", "dummy"\
+                , "armor_stand", "entity"]
